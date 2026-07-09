4b3385473b32345ba8b7bc05df0924067d528475
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2026-07-09 14:46:47 +0530

    fix: dynamically infer baseURL in browser to fix connection refused errors

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react"
 import {emailOTPClient} from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || (typeof window !== "undefined" ? window.location.origin : undefined),
     plugins: [emailOTPClient()]
 })
